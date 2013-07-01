42a0b4a99b4afbfcbd7813f04b02b355d2f38636
Balthazar Rouberol <balthazar.rouberol@mapado.com>
2013-07-01 16:02:47 +0200

    Project renamed to contexttimer (ctimer already exists)
    Signed-off-by: Balthazar Rouberol <balthazar.rouberol@mapado.com>

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
-import ctimer
+import contexttimer
 
 from os.path import join, dirname
 
 from setuptools import setup
 
 setup(
-    name="ctimer",
-    version=ctimer.__version__,
+    name="contexttimer",
+    version=contexttimer.__version__,
     license="GPLv3",
     description='A timer context manager measuring the clock wall time of the code block it contains.',
     author="Balthazar Rouberol",
     author_email='brouberol@imap.cc',
     url='https://github.com/brouberol/ctimer',
-    packages=['ctimer'],
+    packages=['contexttimer'],
     keywords=['time', 'timer'],
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: OS Independent'
+        'Operating System :: OS Independent',
         'Topic :: Utilities',
     ],
     long_description=open(join(dirname(__file__), 'README.rst')).read(),
